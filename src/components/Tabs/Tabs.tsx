443814655695fe223740e253c2ad4bf84bb5bcef
Garett Begnoche <garett.begnoche@techngs.com>
2024-10-24 14:57:43 -0400

    add extending underlines

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,17 +1,50 @@
-import { Link } from "react-router-dom";
+import {
+  To,
+  useLocation,
+  useNavigate,
+  useResolvedPath,
+} from "react-router-dom";
 import "./Tabs.css";
 
 export const Tabs = () => {
-  const tabs = ["about", "contact"];
-
   return (
     <div className="tab-container">
-      {tabs.map((tab) => (
-        <Link to={`/${tab}`} className="tab">
-          {tab}
-        </Link>
-      ))}
+      <Tab className="tab" to="/about">
+        about
+      </Tab>
+      <div className="underline" />
+      <Tab className="tab" to="/contact">
+        contact
+      </Tab>
+      <div className="underline" />
     </div>
   );
 };
+
+export type TabProps = {
+  to: To;
+};
+
+export const Tab: React.FunctionComponent<
+  TabProps & React.HTMLAttributes<HTMLButtonElement>
+> = ({ children, to, className }) => {
+  const navigate = useNavigate();
+
+  const { pathname: toPathname } = useResolvedPath(to);
+  const { pathname: locationPathname } = useLocation();
+
+  const selected = locationPathname.startsWith(toPathname);
+
+  return (
+    <button
+      className={className}
+      role="tab"
+      onClick={() => navigate(to)}
+      aria-selected={selected}
+    >
+      {children}
+    </button>
+  );
+};
+
 export default Tabs;
