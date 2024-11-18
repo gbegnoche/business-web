d7a00aabaf76bb2b2020b0e4f711a4e31ba830c2
Garett Begnoche <garett.begnoche@techngs.com>
2024-11-18 15:58:31 -0500

    add icons and change body styling

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,19 +1,25 @@
+import techngs from "../assets/images/techngs-icon.jpeg";
+import covey from "../assets/images/covey-icon.jpeg";
+
 export type Project = {
   title: string;
   subtitle: string;
   description: string;
+  logo?: string;
   content?: React.ReactNode;
 };
 
 export const Projects = () => {
   const projectList: Project[] = [
     {
+      logo: techngs,
       title: "Technergetics, LLC",
       subtitle: "Software Engineer, Team Lead (2022-2024)",
       description:
         "Led a team of software engineers building an application for the US Air Force. Determined the way forward for team members to ensure application was delivered on time and met quality standards as well as business needs.",
     },
     {
+      logo: covey,
       title: "Covey Computer Software",
       subtitle: "Software Engineer (2018-2021)",
       description:
@@ -25,10 +31,14 @@ export const Projects = () => {
     title,
     subtitle,
     description,
+    logo,
     content,
   }: Project) => (
     <div key={title} className="project-container">
-      <h3>{title}</h3>
+      <span>
+        {logo && <img src={logo} />}
+        <h3>{title}</h3>
+      </span>
       <h4>{subtitle}</h4>
       {content}
       <p>{description}</p>
