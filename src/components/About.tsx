80783d6dac00713e236007f648f114a54b2d6a7a
Garett Begnoche <garett.begnoche@techngs.com>
2024-11-19 20:56:09 -0500

    add missing images and about page content

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,12 +1,28 @@
-import selfie from "../assets/images/self.jpeg";
-
 export const About = () => (
   <div className="about-container">
-    <img src={selfie} alt="me!" />
-    <p>
-      <strong>Garett Begnoche</strong> is a contract software developer with
-      experience in Node.js and React frontend.
-    </p>
+    <h3>About me</h3>
+    <span>
+      <p>
+        <u>Hi! I'm Garett</u>. I'm a <b>Software Engineer</b> based in{" "}
+        <b>Brooklyn, NY</b>. When I was a kid living upstate, I was always
+        fascinated by technology, and I've been creating web pages since 2003.
+        Taking that enthusiasm into my work, I'm passionate about crafting great{" "}
+        <b>user experiences</b>, ensuring <b>accessible applications</b>, and
+        writing code that's easy to read and <b>scalable</b> for organizations
+        looking to grow. My goal is always to create applications that not only
+        look great, but are also usable, performant, and secure.
+      </p>
+      <p>
+        I've worked with a variety of companies in both the public and private
+        sectors. I believe that communication is key to the success of a
+        project, and I work closely with clients to ensure their needs are heard
+        and met through the software I deliver.
+      </p>
+      <p>
+        I also compose music, develop video games, write comedy sketches, and
+        hang out with my cat.
+      </p>
+    </span>
   </div>
 );
 
