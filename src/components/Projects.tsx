199b4f7d5b933a354cd3c1d9ee6846ff07628beb
Garett Begnoche <garett.begnoche@techngs.com>
2024-11-19 22:14:26 -0500

    style projects page and add Indium

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,3 +1,4 @@
+import indium from "../assets/images/indium-icon.jpeg";
 import techngs from "../assets/images/techngs-icon.jpeg";
 import covey from "../assets/images/covey-icon.jpeg";
 
@@ -12,16 +13,23 @@ export type Project = {
 export const Projects = () => {
   const projectList: Project[] = [
     {
+      logo: indium,
+      title: "Indium Corporation of America",
+      subtitle: "2024 - present",
+      description:
+        "Rebuilding legacy application from the ground up to modernize company infrastructure. Working with sites globally to coordinate app deployment.",
+    },
+    {
       logo: techngs,
       title: "Technergetics, LLC",
-      subtitle: "Software Engineer, Team Lead (2022-2024)",
+      subtitle: "2022 - 2024",
       description:
         "Led a team of software engineers building an application for the US Air Force. Determined the way forward for team members to ensure application was delivered on time and met quality standards as well as business needs.",
     },
     {
       logo: covey,
       title: "Covey Computer Software",
-      subtitle: "Software Engineer (2018-2021)",
+      subtitle: "2018 - 2021",
       description:
         "Worked with multiple clients to determine business needs and deliver software solutions. Projects included transitioning customers from legacy software, research and development on a hardware solution for tracking manufacturing processes, and even a company-sponsored game jam.",
     },
@@ -37,11 +45,13 @@ export const Projects = () => {
     <div key={title} className="project-container">
       <span>
         {logo && <img src={logo} />}
-        <h3>{title}</h3>
+        <div>
+          <h3>{title}</h3>
+          <h4>{subtitle}</h4>
+          <p>{description}</p>
+        </div>
       </span>
-      <h4>{subtitle}</h4>
       {content}
-      <p>{description}</p>
     </div>
   );
 
